de9f8597c80b1c714bbe42b2c18a714491e50419
Jin-Hwan Jeong <jhjeong.kr@gmail.com>
2017-03-21 15:51:09 +0900

    coded two interfaces

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
+
 	log "plugin-to-SONA/log"
 
 	//	"github.com/containernetworking/cni/pkg/invoke"
@@ -10,16 +13,42 @@ import (
 )
 
 func cmdAdd(args *skel.CmdArgs) error {
+	log.Info("cni: add")
 	return nil
 }
 
 func cmdDel(args *skel.CmdArgs) error {
+	log.Info("cni: delete")
 	return nil
 }
 
 func main() {
-	log.Info("sona cni is invoked")
-	//	config.ParseCommandLine()
-	skel.PluginMain(cmdAdd, cmdDel, version.All)
-	log.Info("sona cni is terminated")
+	log.Info(os.Args[0], " is invoked")
+	if e := skel.PluginMainWithError(cmdAdd, cmdDel, version.All); e != nil {
+		log.Error(os.Args[0], " returns with the following: ", prettyString(e))
+		if err := printWithReturn(e); err != nil {
+			log.Error("Error writing error JSON to stdout: ", err)
+		}
+	} else {
+		log.Info(os.Args[0], " returns the result: ")
+	}
+	os.Exit(1)
+}
+
+// This function is modified from .../cni/pkg/types.Result object
+func printWithReturn(obj interface{}) error {
+	data, err := json.MarshalIndent(obj, "", "    ")
+	if err != nil {
+		return err
+	}
+	_, err = os.Stdout.Write(append(data, ([]byte("\n"))...))
+	return err
+}
+
+func prettyString(obj interface{}) string {
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return "conversion error"
+	}
+	return string(data)
 }
